65130c5006c05674a28e7f7614d650c3af1f12a7
zoeyzuo1126 <jiahui.zuo@emory.edu>
2021-11-04 14:18:35 -0400

    new folder

---
+++ R/graphs.R
@@ -0,0 +1,16 @@
+here::i_am("R/graphs.R")
+data(mtcars)
+
+
+library(dplyr)
+dataset<-mtcars
+#grosspower=mpg+wt+am
+#am is a factor
+dataset$am<-factor(dataset$am)
+#head(dataset)
+#select the variables we need
+dataset1<-dataset%>%select(1,4,6,9)
+fit<-lm(hp~mpg+wt+am,data=dataset1)
+summary(fit)
+par(mfrow=c(2,2))
+plot(fit)
