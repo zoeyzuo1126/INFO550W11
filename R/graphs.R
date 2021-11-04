61d387c8f4794da68a308e5709f79e03d70e7e61
zoeyzuo1126 <78101100+zoeyzuo1126@users.noreply.github.com>
2021-11-04 14:58:51 -0400

    Update graphs.R

--- R/graphs.R
+++ R/graphs.R
@@ -7,14 +7,17 @@ dataset<-mtcars
 #grosspower=mpg+wt+am
 #am is a factor
 dataset$am<-factor(dataset$am)
-#head(dataset)
+
 #select the variables we need
 dataset1<-dataset%>%select(1,4,6,9)
+#fit into model
 fit<-lm(hp~mpg+wt+am,data=dataset1)
+#check the results
 summary(fit)
 par(mfrow=c(2,2))
 plot(fit)
 
+## export graph
 png(here::here("figs", "graphs.png"))
 par(mfrow=c(2,2))
 plot(fit)
